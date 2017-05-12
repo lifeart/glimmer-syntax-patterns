92b329d51fb04c0980aa9c5588bd6fb16f8b6ce1
Dan Hedgecock <hedgecock.d@gmail.com>
2017-05-12 09:06:45 -0700

    Update build, fix handlebars comment pattners :sparkles:

--- build.js
+++ build.js
@@ -2,58 +2,90 @@
 const plist = require('plist');
 const { readFileSync, writeFile } = require('fs');
 
-const glimmerPatterns = require('./glimmer.tmLanguage.json');
-let typeScriptBase = readFileSync('./TypeScript.tmLanguage', 'utf-8');
-let javaScriptBase = require('./node_modules/language-babel/grammars/Babel Language.json');
+const glimmerPatterns = require('./grammars/glimmer.tmLanguage.json');
+let typeScriptBase = readFileSync('./grammars/TypeScriptReact.tmLanguage', 'utf-8');
+
+function fixGrammarScopeNames(rule, target) {
+  if (typeof rule.name === 'string') {
+    rule.name = target === 'ts' ?
+      rule.name.replace(/\.tsx/g, '.ts')
+      : rule.name.replace(/\.ts/g, '.js');
+  }
+
+  if (typeof rule.contentName === 'string') {
+    rule.contentName = target === 'ts' ?
+      rule.contentName.replace(/\.tsx/g, '.ts')
+      : rule.contentName.replace(/\.ts/g, '.js');
+  }
+  // recurse
+  for (let property in rule) {
+    let value = rule[property];
+    if (typeof value === 'object') {
+      fixGrammarScopeNames(value, target);
+    }
+  }
+}
 
 // hbs`` pattern used to enter glimmer syntax inside of JS/TS files
 const glimmerTaggedTemplatePattern = {
-  name: 'meta.source.glimmer.ts',
+  name: 'meta.source.handlebars.ts',
   begin: '\\s?(hbs)((`))', // Optional whitespace required for Babel to match pattern
   end: '((`))',
   beginCaptures: {
     '1': { name: 'entity.name.tag.ts' },
-    '2': { name: 'markup.template.definition.begin.glimmer.ts' },
+    '2': { name: 'markup.template.definition.begin.handlebars.ts' },
     '3': { name: 'string.template.tagged.ts' }
   },
   endCaptures: {
-    '1': { name: 'markup.template.definition.end.glimmer.ts' },
+    '1': { name: 'markup.template.definition.end.handlebars.ts' },
     '2': { name: 'string.template.tagged.ts' }
   },
   patterns: [
-    { include: '#glimmer-unescaped-expression' },
-    { include: '#glimmer-comment' },
-    { include: '#glimmer-expression' },
-    { include: '#html-tag' },
-    { include: '#html-comment' },
-    { include: '#entities' }
+    { include: 'source.handlebars' }
   ]
 };
 
+// Write Files
+writeFile('dist/Glimmer.tmLanguage.json', JSON.stringify(glimmerPatterns, null, 2), err => {
+  if (err) { return console.warn(err); }
+  console.log('Glimmer build complete');
+});
+
 // Update TypeScript base with Glimmer patterns
 typeScriptBase = plist.parse(typeScriptBase);
-typeScriptBase.name = 'TypeScript (Glimmer)';
+delete typeScriptBase.uuid; // TODO: make new uuids
+typeScriptBase.name = 'TypeScript (Extended)';
+typeScriptBase.scopeName = 'source.ts';
+typeScriptBase.fileTypes = ['ts'];
 typeScriptBase.repository['glimmer-tagged-template'] = glimmerTaggedTemplatePattern;
 typeScriptBase.repository.expression.patterns.unshift({ include: '#glimmer-tagged-template' });
 
-// Update JavaScript base with Glimmer patterns
-javaScriptBase.name = 'JavaScript (Babel+Glimmer)';
-javaScriptBase.scopeName = 'source.js';
-javaScriptBase.repository['glimmer-tagged-template'] = glimmerTaggedTemplatePattern;
-javaScriptBase.repository.expression.patterns.unshift({ include: '#glimmer-tagged-template' });
-
-// Inject all Glimmer patterns into pattern repositories
-for (let pattern in glimmerPatterns.repository) {
-  typeScriptBase.repository[pattern] = glimmerPatterns.repository[pattern];
-  javaScriptBase.repository[pattern] = glimmerPatterns.repository[pattern];
+// Fix tsx => ts
+for (let key in typeScriptBase.repository) {
+  fixGrammarScopeNames(typeScriptBase.repository[key], 'ts');
 }
 
+typeScriptBase.patterns.forEach(pattern => fixGrammarScopeNames(pattern, 'ts'));
+
 // Write Files
-writeFile('dist/TypeScriptGlimmer.tmLanguage.json', JSON.stringify(typeScriptBase, null, 2), err => {
+writeFile('dist/TypeScriptExtended.tmLanguage.json', JSON.stringify(typeScriptBase, null, 2), err => {
   if (err) { return console.warn(err); }
   console.log('TS build complete');
 });
-writeFile('dist/JavaScriptGlimmer.tmLanguage.json', JSON.stringify(javaScriptBase, null, 2), err => {
+
+// Update JavaScript base with Glimmer patterns
+typeScriptBase.name = 'JavaScript (Extended)';
+typeScriptBase.scopeName = 'source.js';
+typeScriptBase.fileTypes = ['js', 'jsx'];
+
+// Fix ts => js
+for (let key in typeScriptBase.repository) {
+  fixGrammarScopeNames(typeScriptBase.repository[key], 'js');
+}
+
+typeScriptBase.patterns.forEach(pattern => fixGrammarScopeNames(pattern, 'js'));
+
+writeFile('dist/JavaScriptExtended.tmLanguage.json', JSON.stringify(typeScriptBase, null, 2), err => {
   if (err) { return console.warn(err); }
   console.log('JS build complete');
 });
